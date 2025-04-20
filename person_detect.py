b77003a52496141339b2cef9046b9c963b13f9e2
larifreitas <b.larissafs@gmail.com>
2025-04-20 10:50:17 -0300

    pre processamento para melhorar detecção

--- person_detect.py
+++ person_detect.py
@@ -10,22 +10,30 @@ cap = cv2.VideoCapture(str(video))
 
 
 while True:
-    ret, image_np = cap.read()
+    ret, frame = cap.read()
     if not ret: break
+
+    # pré processamento e melhoria para visão noturna ou ambiente escuro para detecção
+    frame = cv2.resize(640,640)
+    frame = cv2.cvtColor(frame,cv2.COLOR_BGR2LAB)
+    l,a,b = cv2.split(frame)
+    l_eq = cv2.equalizeHist(l)
+    frame = cv2.merge(l_eq,a,b)
+    frame = cv2.cvtColor(frame,cv2.COLOR_LAB2BGR)
     
     # Detecção
-    results = model(image_np)
+    results = model(frame)
     
     # Bbox
     for r in results:
         for bbox, cls, conf in zip(r.boxes.xyxy, r.boxes.cls, r.boxes.conf):
             if int(cls) == num_class and conf > 0.7:
                 x1, y1, x2, y2 = map(int,bbox)
-                cv2.rectangle(image_np, (x1, y1), (x2, y2), (0,255,130),2)
-                cv2.putText(image_np,f"Person {conf:.2f}",(x1,y1-10),cv2.FONT_HERSHEY_SIMPLEX,0.6,(0,255,130),2)
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0,255,130),2)
+                cv2.putText(frame,f"Person {conf:.2f}",(x1,y1-10),cv2.FONT_HERSHEY_SIMPLEX,0.6,(0,255,130),2)
     
     # exibição
-    cv2.imshow("Person Detection", image_np)
+    cv2.imshow("Person Detection", frame)
     if cv2.waitKey(1) & 0xFF in [ord("q"), 27]:
         break
 
